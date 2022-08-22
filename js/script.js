0877af715ecb9ad9fea827be714ca6357368c5ed
Your Name <anneserocki@gmail.comom>
2022-08-21 19:47:26 -0700

    commit05-03

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Preventing the Browser's Default Behavior
+document.preventDefault();
+
+// Variables
+let btn = document.getElementById("submit");
+
+// Functions
+function saveEmployee() {
+
+}
+
+// Listenners
+btn.addEventListener(click, () => console.log(''));
