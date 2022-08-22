1345ec0fe18c00e41f5dff8199a0c7ded7d586a7
Your Name <anneserocki@gmail.comom>
2022-08-21 22:30:23 -0700

    commit05-04

--- js/script.js
+++ js/script.js
@@ -1,13 +1,37 @@
 // Preventing the Browser's Default Behavior
-document.preventDefault();
+document.preventDefault;
 
 // Variables
+let employeeForm = document.getElementById("empForm");
+let digitId = document.getElementById("id");
+let empName = document.getElementById("name");
+let digitExt = document.getElementById("ext");
+let empEmail = document.getElementById("email");
+let empDept = document.getElementById("department"); 
 let btn = document.getElementById("submit");
 
 // Functions
-function saveEmployee() {
+function helper() {
+    digitId ="";
+    empName ='';
+    digitExt ="";
+    empEmail ="";
+    empDept ="";
+};
 
-}
+function saveEmployee() {
+    if (window.confirm('Are you sure to submit?')) {
+        console.log('Saved successfully.')
+        console.log(digitId);
+        console.log(empName);
+        console.log(digitExt);
+        console.log(empEmail);
+        console.log(empDept);
+    } else {
+        console.log('Canceled.');
+    };
+};
 
 // Listenners
-btn.addEventListener(click, () => console.log(''));
+employeeForm.addEventListener('load', helper);
+btn.addEventListener('click', saveEmployee, false);
